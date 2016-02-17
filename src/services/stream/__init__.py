a4656dca547f104ca35649e7e3cab49c059dc012
Enigma <enigma@enigmablade.net>
2016-02-17 02:08:03 -0500

    Export service modules dynamically from their packages

--- src/services/stream/__init__.py
+++ src/services/stream/__init__.py
@@ -1,1 +1,8 @@
-from . import crunchyroll, funimation
+# Export all valid modules from the package
+import glob, os
+module_dir = os.path.dirname(__file__)
+modules = glob.glob(module_dir+"/*.py")
+__all__ = [os.path.basename(f)[:-3] for f in modules if os.path.isfile(f) and not os.path.basename(f).startswith("_")]
+del os
+
+from . import *
