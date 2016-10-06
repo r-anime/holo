07d56cce7cd338c57d49fbe77b22819fb3d06d39
Enigma <theenigmablade@gmail.com>
2016-10-06 14:16:34 -0400

    Add daisuki link support

--- src/services/stream/daisuki.py
+++ src/services/stream/daisuki.py
@@ -1,13 +1,19 @@
 # Shows in a region: https://motto.daisuki.net/api2/search/mode:1
 # Specific show: https://motto.daisuki.net/api2/seriesdetail/SHOW_ID
+# Public show page: http://www.daisuki.net/us/en/anime/detail.SHOW_KEY.html
 
 from logging import debug, info, warning, error
 from datetime import datetime
+import re
 
 from .. import AbstractServiceHandler
 from data.models import Episode
 
 class ServiceHandler(AbstractServiceHandler):
+	_show_url = "http://funimation.com/shows/{key}"
+	
+	_show_key_re = re.compile("daisuki\.net/[a-z]{2}/[a-z]{2}/anime/detail\.([^/.]+)(?:\.html)?", re.I)
+	
 	def __init__(self):
 		super().__init__("daisuki", "Daisuki", False)
 	
@@ -27,7 +33,10 @@ class ServiceHandler(AbstractServiceHandler):
 	# Local info formatting
 	
 	def get_stream_link(self, stream):
-		return None
+		return self._show_url.format(key=stream.show_key)
 	
 	def extract_show_key(self, url):
+		match = self._show_key_re.search(url)
+		if match:
+			return match.group(1)
 		return None
