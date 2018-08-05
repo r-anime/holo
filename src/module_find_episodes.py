2bfe72ff20bdb2e3fdd8205af2e330c14d0caa34
Arnaud Dethise <arnauddethise@gmail.com>
2018-08-05 08:11:39 +0300

    fixed empty score links

--- src/module_find_episodes.py
+++ src/module_find_episodes.py
@@ -217,7 +217,7 @@ def _gen_text_discussions(db, formats, show, stream):
 			else:
 				score = poll_handler.get_score(poll)
 				poll_link = poll_handler.get_results_link(poll)
-			table.append(safe_format(formats["discussion"], episode=episode.number, link=episode.link, score=score if score else "", poll_link=poll_link if poll_link else ""))
+			table.append(safe_format(formats["discussion"], episode=episode.number, link=episode.link, score=score if score else "", poll_link=poll_link if poll_link else "http://localhost")) # Need valid link even when empty
 
 		num_columns = 1 + (len(table) - 1) // 20
 		format_head, format_align = formats["discussion_header"], formats["discussion_align"]
