4c6fe991b412b4ba30032ea9c27085cac9b354da
Bainos <bainos.zzz@gmail.com>
2020-07-10 18:20:08 +0000

    ensuring that streams created with from_show have different ids if they refer to different shows

--- src/data/models.py
+++ src/data/models.py
@@ -117,7 +117,7 @@ class Stream(DbEqMixin):
 	
 	@classmethod
 	def from_show(cls, show):
-		return Stream(-1, -1, show, show.id, -1, show.name, 0, 0, 1)
+		return Stream(-show.id, -1, show, show.id, -1, show.name, 0, 0, 1)
 	
 	def to_internal_episode(self, episode):
 		e = copy.copy(episode)
