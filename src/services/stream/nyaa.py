75f68a7d413f71fccbf5376c7b4aeaae46745626
Bielna <bainos.zzz@gmail.com>
2023-01-29 15:52:08 +0000

    update some-stuff parser regex

--- src/services/stream/nyaa.py
+++ src/services/stream/nyaa.py
@@ -205,7 +205,7 @@ _num_extractors = [re.compile(x, re.I) for x in [
 	# " - " separator between show and episode
 	r"\[(?:horriblesubs|SubsPlease|commie|hiryuu|kuusou|fff|merchant|lolisubs|hitoku|erai-raws|davinci|asenshi|mezashite|anonyneko|pas|ryuujitk|rip time)\] .+ - (\d+) ",
 	r"\[DameDesuYo\] .+ - (\d+)[ v]",
-	r"\[Some-Stuffs\]_.+_(\d{3})_",
+	r"\[Some-Stuffs\] .+ (\d{3}) ",
 	r"\[(?:orz|hayaku|sxrp)\] .+ (\d+)", # No separator
 	r"\[(?:kaitou|gg)\]_.+_-_(\d+)_", # "_-_" separator
 	r"\[flysubs].+ - (\d+)\[.+\]", # "_-_" separator
